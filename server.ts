cdd432fb6b02bdb765261a6bb12adbcb7a2306fa
Mike Weaver <michael@triplewhale.com>
2023-06-22 15:29:28 -0400

    Nicer layout n stuff

--- server.ts
+++ server.ts
@@ -5,34 +5,38 @@ import chalk from 'chalk'
 import moment from 'moment'
 import jwt from 'jsonwebtoken'
 import { expressjwt, Request as JWTRequest } from 'express-jwt'
+import { v4 as uuidv4 } from 'uuid'
 
 // -----------------------
 // express app
 // -----------------------
 const app = express()
 const port = 3000
-const appName = chalk.hex('#1877f2')('[triple-whale] ')
+const appName = chalk.blue('[🐳🐳🐳] ')
 app.use(express.json())
 
 dotenv.config()
 const { NODE_ENV } = process.env
 
 // -----------------------
-// secret key
+// jwt secret
+// how do we handle this in production?
 // -----------------------
-const secret = 'password'
+const secret = uuidv4()
 
+// -----------------------
+// jwt login
+// -----------------------
 app.post('/login', (req: JWTRequest, res: express.Response) => {
-  const { username = '', password = '' } = req.body
-  if (username === '' || password === '' || password !== secret)
-    return res.sendStatus(401)
+  const { username = '' } = req.body
+  if (username === '') return res.sendStatus(401)
 
-  jwt.sign({ username, password }, secret, (err: any, token: any) => {
+  jwt.sign({ username }, secret, (err: any, token: any) => {
     if (err) {
       return res.sendStatus(500)
     }
 
-    res.json({ token })
+    res.json({ token, secret })
   })
 })
 
@@ -43,7 +47,12 @@ app.get(
   '/protected',
   expressjwt({ secret, algorithms: ['HS256'] }),
   function (req: JWTRequest, res: express.Response) {
-    res.json({ message: '🤫 the password is password' })
+    res.json({
+      message: `🤫 the password is ${secret}`,
+      data: {
+        secret,
+      },
+    })
   }
 )
 
@@ -57,10 +66,8 @@ app.get('/day', (req: Request, res: Response) => {
 const loggy = () => {
   console.log(
     appName +
-      chalk.green(
-        `🐳🐳🐳 listening http://localhost:${
-          NODE_ENV === 'production' ? '80' : port
-        }`
+      chalk.blue(
+        `listening http://localhost:${NODE_ENV === 'production' ? '80' : port}`
       )
   )
 }
--- src/jwt.tsx
+++ src/jwt.tsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react'
 import jwt_decode from 'jwt-decode'
 import axios from 'axios'
 import { Button } from './component-library/Button/Button'
+import { InputBase, Stack, Text, Space, Card, Container } from '@mantine/core'
 
 export default function JWTComponent() {
   const [token, setToken] = useState('')
   const [protectedData, setProtectedData] = useState('')
+  const [username, setUsername] = useState('')
 
   const handleLogin = async () => {
     try {
       const response = await axios.post('/login', {
-        username: 'mike',
-        password: 'password',
+        username,
       })
       const { token } = response.data
       setToken(token)
@@ -37,22 +38,53 @@ export default function JWTComponent() {
     setProtectedData('')
   }
 
-  const decodedToken = token ? jwt_decode(token) : null
+  const decodedToken: any = token ? jwt_decode(token) : null
 
   return (
-    <div className="App">
-      {token ? (
-        <div>
-          <p>Welcome, {JSON.stringify(decodedToken)}!</p>
-          <p>Protected Data: {protectedData}</p>
-          <Button onClick={handleGetProtectedData}>Get Protected Data</Button>
-          <Button variant="secondary" onClick={handleLogout}>
-            Logout
-          </Button>
-        </div>
-      ) : (
-        <Button onClick={handleLogin}>Login</Button>
-      )}
-    </div>
+    <Container size="xs">
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        {token ? (
+          <>
+            <Text fz="lg" fw="bold">
+              Welcome, {decodedToken.username}!
+            </Text>
+            {protectedData && (
+              <>
+                <Text fz="md">Protected Data:</Text>
+                <Text fs="italic">{protectedData}</Text>
+              </>
+            )}
+            <Space h="md" />
+            <Stack>
+              <Button onClick={handleGetProtectedData}>
+                Get Protected Data
+              </Button>
+              <Button variant="secondary" color="red.5" onClick={handleLogout}>
+                Logout
+              </Button>
+            </Stack>
+          </>
+        ) : (
+          <>
+            <Text fz="lg">Please login to view protected data!</Text>
+            <Space h="md" />
+            <Stack>
+              <InputBase
+                onChange={(e) => setUsername(e.target.value)}
+                onKeyUp={(e) => {
+                  if (e.key === 'Enter') {
+                    handleLogin()
+                  }
+                }}
+                placeholder="Enter your name"
+              />
+              <Button disabled={username === ''} onClick={handleLogin}>
+                Login
+              </Button>
+            </Stack>
+          </>
+        )}
+      </Card>
+    </Container>
   )
 }
