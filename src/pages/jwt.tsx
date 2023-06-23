5886fd7ba7892b1e53dd302db5eab2fc295df2d7
Mike Weaver <michael@triplewhale.com>
2023-06-23 09:41:21 -0400

    cleanup

--- src/pages/jwt.tsx
+++ src/pages/jwt.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import jwt_decode from 'jwt-decode'
 import axios from 'axios'
 import { Button } from '../component-library/Button/Button'
